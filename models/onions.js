ccb46ed833df1299c4418256f5341c54126a6319
TejaswiniPydipally <143117469+TejaswiniPydipally@users.noreply.github.com>
2023-11-29 22:03:11 -0600

    added files

--- models/onions.js
+++ models/onions.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose")
 const onionsSchema = mongoose.Schema({
 onions_type: String,
 Onions_quantity: String,
-onions_cost: Number
+
+onions_cost:  {
+    type: Number,
+    required: true,
+    min: [0, 'Price must be at least 0'],
+    max: [100, 'Price cannot exceed 100'],
+  }
 })
 module.exports = mongoose.model("onions", 
 onionsSchema)
--- routes/onions.js
+++ routes/onions.js
@@ -1,29 +1,25 @@
 var express = require('express');
+const onions_controllers= require('../controllers/onions');
 var router = express.Router();
-const onions_controllers = require('../controllers/onions');
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('onions', { title: 'Search Result onions' });
-});
-
+const passport = require('passport');
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+/* GET onionss */
+router.get('/', onions_controllers.onions_view_all_Page);
 /* GET detail onions page */
-router.get('/detail', onions_controllers.onions_view_one_Page);
-
+router.get('/detail', secured,  onions_controllers.onions_view_one_Page);
 /* GET create onions page */
-router.get('/create', onions_controllers.onions_create_Page);
-
+router.get('/create', secured,  onions_controllers.onions_create_Page);
 /* GET create update page */
-router.get('/update', onions_controllers.onions_update_Page);
-
+router.get('/update', secured, onions_controllers.onions_update_Page);
 /* GET delete onions page */
-router.get('/delete', onions_controllers.onions_delete_Page);
-router.get('/create', onions_controllers.onions_create_Page);
-/* GET create update page */
-router.get('/update', onions_controllers.onions_update_Page);
-/* GET delete costume page */
-router.get('/delete', onions_controllers.onions_delete_Page);
-
-
+router.get('/delete', secured,  onions_controllers.onions_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });   
 
 module.exports = router;
